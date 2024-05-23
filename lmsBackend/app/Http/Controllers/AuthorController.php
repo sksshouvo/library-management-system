37abe67fcac613b311895e775474f0002c466bb8
Salman Kabir <sksshouvo2@gmail.com>
2024-05-23 18:43:02 +0600

    Working on Author Rest Api

--- lmsBackend/app/Http/Controllers/AuthorController.php
+++ lmsBackend/app/Http/Controllers/AuthorController.php
@@ -11,23 +11,26 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    public function __construct(public AuthorRepository $authorRepository) {
+
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return $this->successResponse(__('author.list'), $this->authorRepository->list()->getData(), $request->bearerToken(), 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(AuthorStoreRequest $request, AuthorRepository $authorService): JsonResponse
+    public function store(AuthorStoreRequest $request): JsonResponse
     {
         try {
             
             DB::beginTransaction();
-            $author = $authorService->store($request->validated());
+            $author = $this->authorRepository->store($request->validated());
             DB::commit();
             return $this->successResponse(__('author.created'), $author->getData(), $request->bearerToken(), 201);
         } catch (\Exception $e) {
@@ -42,7 +45,13 @@ class AuthorController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $singleAuthor = $this->authorRepository->single($id)->getData();
+        if ($singleAuthor) {
+            return $this->successResponse(__('author.list'), $singleAuthor, $request->bearerToken(), 200);
+        } else {
+            return $this->errorResponse(__('author.invalid_id'), NULL, $request->bearerToken(), 422);
+        }
+
     }
 
     /**
@@ -50,7 +59,16 @@ class AuthorController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $author = $this->authorRepository->update($request->validated(), $id);
+            DB::commit();
+            return $this->successResponse(__('author.updated'), $author->getData(), $request->bearerToken(), 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::emergency($e);
+            return $this->errorResponse(__('common.error'), $e, $request->bearerToken());
+        }
     }
 
     /**
@@ -58,6 +76,20 @@ class AuthorController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $author = $this->authorRepository->delete($id);
+            DB::commit();
+            if ($author->getData()) {
+                return $this->successResponse(__('author.deleted'), NULL, $request->bearerToken(), 200);
+            } else {
+                return $this->errorResponse(__('author.invalid_id'), NULL, $request->bearerToken());
+            }
+            
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::emergency($e);
+            return $this->errorResponse(__('common.error'), $e, $request->bearerToken());
+        }
     }
 }
